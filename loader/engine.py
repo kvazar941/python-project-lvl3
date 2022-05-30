aebead6ea33975887ff5efa9328d6220e649bdcc
Sergey <kvazar941@gmail.com>
2022-05-30 15:13:57 +0300

    released downloading images

--- loader/engine.py
+++ loader/engine.py
@@ -1,7 +1,26 @@
 """Engine module."""
 #import os
+from html.parser import HTMLParser
+from bs4 import BeautifulSoup
 import requests
 from loader.file_writer import write
+
+
+def rename(name):
+    '''https://ru.hexlet.io/courses --> ru-hexlet-io-courses'''
+    one = name.split('//')[1]
+    two = one.replace('.', '-')
+    return two.replace('/', '-')
+
+
+def rename_to_html(name):
+    res_name = rename(name)
+    return res_name + '.html' if res_name[-4:] != 'html' else res_name
+
+
+def rename_to_png(name):
+    res_name = rename(name)
+    return res_name + '.png' if res_name[-3:] != 'png' else res_name
 
 
 class Page():
@@ -10,11 +29,7 @@ class Page():
 
     def valid_name(self):
         '''https://ru.hexlet.io/courses --> ru-hexlet-io-courses.html'''
-        one = (self.url).split('//')[1]
-        two = one.replace('.', '-')
-        three = two.replace('/', '-')
-        file_name = three + '.html' if three[-4:] != 'html' else three
-        return file_name
+        return rename_to_html(self.url)
 
     def response(self):
         return requests.get(self.url)
@@ -25,6 +40,10 @@ class Page():
 
 def page_load(url_page, way_to_dir):
     page = Page(url_page)
-    write(''.join([way_to_dir, page.valid_name()]), page.content_url())
+    #write(''.join([way_to_dir, page.valid_name()]), page.content_url())
+    soup = BeautifulSoup(page.content_url(), 'html.parser')
+    link = [x['src'] for x in soup.find_all('img')]
+    #print(soup.find_all('img'))
+    print(link)
     return str(way_to_dir + page.valid_name())
 
