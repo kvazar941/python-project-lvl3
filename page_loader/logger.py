ecd4fe757b262e7f3304a9c2e5bf097d00e45350
Sergey <kvazar941@gmail.com>
2022-06-29 00:18:43 +0300

    add test make directory error

--- page_loader/logger.py
+++ page_loader/logger.py
@@ -12,7 +12,6 @@ NAME_REPORT_DEBUG = 'report_debug.log'
 def log(message):
     make_directory(DIRECTORY_LOGS)
     logger = create_logger(DIRECTORY_LOGS)
-    logger.info(message)
     logger.debug(message)
 
 
@@ -28,13 +27,9 @@ def create_logger(directory):
     """
     logger = logging.getLogger('log')
     logger.setLevel(logging.DEBUG)
-    fi = logging.FileHandler(''.join([directory, NAME_REPORT_INFO]))
-    fi.setLevel(logging.INFO)
     fd = logging.FileHandler(''.join([directory, NAME_REPORT_DEBUG]))
     fd.setLevel(logging.DEBUG)
     formatter = logging.Formatter(LOGGER_FORMAT)
-    fi.setFormatter(formatter)
     fd.setFormatter(formatter)
-    logger.addHandler(fi)
     logger.addHandler(fd)
     return logger
