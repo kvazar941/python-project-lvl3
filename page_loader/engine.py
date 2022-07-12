35256400ff357e78fab0c263b3142f29210ab47d
Sergey <kvazar941@gmail.com>
2022-07-12 15:03:00 +0300

    edits on comments #1

--- page_loader/engine.py
+++ page_loader/engine.py
@@ -1,6 +1,6 @@
 """engine module."""
 import os
-from urllib.parse import urlparse, urlunparse
+from urllib.parse import urlparse
 
 from progress.bar import Bar
 
@@ -8,20 +8,34 @@ from page_loader.checker import check_way
 from page_loader.data_recipient import get_data
 from page_loader.downloader import download_file, download_html, make_directory
 from page_loader.logger import log_debug, log_info
-from page_loader.page_object import get_links
+from page_loader.page_object import get_links, restore_links
 from page_loader.renamer import rename_to_dir, rename_to_file, rename_to_html
 
 DEFAULT_WAY = os.getcwd()
 
 
-def get_full_link(url, short_link):
-    url_link = urlparse(url)
-    url_short = urlparse(short_link)
-    return url_short._replace(scheme=url_link.scheme, netloc=url_link.netloc)
+def get_dict_replased_link(full_links, all_link, page_directory):
+    replased_link = {}
+    for link_new, link_old in zip(full_links, all_link):
+        replased_link[link_old] = rename_to_file(page_directory, link_new)
+    return replased_link
 
 
-def restore_links(url, links):
-    return [urlunparse(get_full_link(url, link)) for link in links]
+def changed_link(dict_changed, content_html):
+    """
+    Replace the links in the html content with local ones.
+
+    Args:
+        dict_changed: dict
+        content_html: str
+
+    Returns:
+        str
+    """
+    new_content = content_html
+    for link in dict_changed:
+        new_content = new_content.replace(link, dict_changed[link])
+    return new_content
 
 
 def get_sourses(list_links, directory):
@@ -45,21 +59,10 @@ def get_sourses(list_links, directory):
     log_debug('Get_sourses complete.')
 
 
-def changed_link(dict_changed, content_html):
-    """
-    Replace the links in the html content with local ones.
-
-    Args:
-        dict_changed: dict
-        content_html: str
-
-    Returns:
-        str
-    """
-    new_content = content_html
-    for link in dict_changed:
-        new_content = new_content.replace(link, dict_changed[link])
-    return new_content
+def save_html(way, text):
+    log_debug('Download html, way: {0}.'.format(way))
+    download_html(way, text)
+    log_debug('Html downloaded.')
 
 
 def load_one_page(url, way):
@@ -75,21 +78,16 @@ def load_one_page(url, way):
     """
     log_debug('Run load one page.')
     page = get_data(url)
-    netloc = urlparse(url).netloc
-    
-    page_directory = rename_to_dir(url)
+    page_dir = rename_to_dir(url)
     way_html = '/'.join([way, rename_to_html(url)])
-    directory_sourses = '/'.join([way, page_directory])
-    all_link = get_links(page.text, netloc)
+    directory_sourses = '/'.join([way, page_dir])
+    all_link = get_links(page.text, urlparse(url).netloc)
     full_links = restore_links(url, all_link)
-    replased_link = {}
+    dict_replased = {}
     if full_links:
-        for link_new, link_old in zip(full_links, all_link):
-            replased_link[link_old] = rename_to_file(page_directory, link_new)
+        dict_replased = get_dict_replased_link(full_links, all_link, page_dir)
         get_sourses(full_links, directory_sourses)
-    log_info('Download html, way: {0}.'.format(way_html))
-    download_html(way_html, changed_link(replased_link, page.text))
-    log_debug('Html downloaded.')
+    save_html(way_html, changed_link(dict_replased, page.text))
     log_debug('Loaded one page.')
     return str(way_html)
 
--- page_loader/page_object.py
+++ page_loader/page_object.py
@@ -1,5 +1,5 @@
 """page_object module."""
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urlunparse
 
 from bs4 import BeautifulSoup
 
@@ -26,3 +26,13 @@ def get_links(content_html, netloc):
     tags = soup.find_all(pre_filter_tag)
     links = [tag[atr] for tag in tags for atr in ATTRIBUTES if tag.get(atr)]
     return [link for link in links if urlparse(link).netloc in {netloc, ''}]
+
+
+def get_full_link(url, short_link):
+    url_link = urlparse(url)
+    url_short = urlparse(short_link)
+    return url_short._replace(scheme=url_link.scheme, netloc=url_link.netloc)
+
+
+def restore_links(url, links):
+    return [urlunparse(get_full_link(url, link)) for link in links]
